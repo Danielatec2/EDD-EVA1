597977adfc91693e08cbd13bf68574f12ff8662f
Daniela Piñón Polanco <124320997+Danielatec2@users.noreply.github.com>
2023-02-21 09:41:02 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_metodos_1;
+
+/**
+ *
+ * @author danii
+ */
+public class EVA1_7_METODOS_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//cabezera
+        // TODO code application logic here
+        myMethod();// invocar al método: llama a funcióm
+        
+    }
+    public static void myMethod(){
+        System.out.println("You have called me : my name is myMethod!!");
+    }
+    
+}
