cb78e66a096a4f01e49cded1c911a7d1e2c0dd5c
Daniela Piñón Polanco <124320997+Danielatec2@users.noreply.github.com>
2023-02-23 09:49:31 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_15_sobrecarga;
+
+/**
+ *
+ * @author danii
+ */
+public class EVA1_15_SOBRECARGA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //DIFERENTES PRINTL :SON 10
+        // METODOS PARA IMPRIMIR DIFERENTES TIPOS DE DATOS
+        //PARA CREAR SOBRECARGA DE METODOS :
+        // LA FIRMA DEL METODO DEBE SER UNICA
+        System.out.println("hola mundo");
+        System.out.println(500);
+        System.out.println(true);
+        System.out.println(args );
+        
+        
+    }
+    
+}
